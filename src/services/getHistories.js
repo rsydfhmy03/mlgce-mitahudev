7d0924371975e50e9a2fd78903a5645d1a28310c
Fahmy rosyadi <95423605+rsydfhmy03@users.noreply.github.com>
2024-05-24 00:11:00 +0700

    Update getHistories.js

--- src/services/getHistories.js
+++ src/services/getHistories.js
@@ -2,7 +2,7 @@ const { Firestore } = require('@google-cloud/firestore');
 
 async function getHistories() {
   const db = new Firestore();
-  const snapshot = await db.collection('prediction').get();
+  const snapshot = await db.collection('predictions').get();
   const histories = [];
   snapshot.forEach(doc => {
     histories.push({ id: doc.id, history: doc.data() });
